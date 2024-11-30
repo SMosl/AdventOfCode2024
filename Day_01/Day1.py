4ce152f7cfefd3c5e970219ae2762a404435c93b
Sam Mosley <sam.mosley@gmail.com>
2024-11-30 13:25:53 +0000

    Set up Day 1 format

---
+++ Day_01/Day1.py
@@ -0,0 +1,17 @@
+import os
+import time
+
+def main(part):
+
+	solution = part
+	dir_path = os.path.dirname(os.path.realpath(__file__))
+	with open(f"{dir_path}/input.txt", "r") as f:
+		return solution
+
+if __name__ == "__main__":
+	start_time = time.time()
+	print(f" Part 1 solution: {main(1)}")
+	print("Part 1 finished --- %s seconds ---" % (time.time() - start_time))
+	mid_time = time.time()
+	print(f" Part 2 solution: {main(2)}")
+	print("Part 2 finished --- %s seconds ---" % (time.time() - mid_time))
