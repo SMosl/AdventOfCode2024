dd559f89538389abf200a209bc8e06adb03e20b6
Sam Mosley <sam.mosley@gmail.com>
2024-12-03 10:30:38 +0000

    Update Day 3 Solution

--- Day_03/solution.py
+++ Day_03/solution.py
@@ -6,21 +6,20 @@ def main(part):
 	solution = 0
 	dir_path = os.path.dirname(os.path.realpath(__file__))
 	with open(f"{dir_path}/input.txt", "r") as f:
+		text = f.read().replace('\n', '')
 		if part == 1:
-			while(len(line := f.readline()) != 0):
-				# Find all occurrences of 'mul(x,y)' where x and y are numbers
-				valid_formats = re.findall(r'mul\((\d+)\,(\d+)\)', line)
-				for pair in valid_formats:
-					solution += int(pair[0]) * int(pair[1])
+			# Find all occurrences of 'mul(x,y)' where x and y are numbers
+			valid_formats = re.findall(r'mul\((\d+)\,(\d+)\)', text)
+			for pair in valid_formats:
+				solution += int(pair[0]) * int(pair[1])
 			return solution
 		else:
-			text = f.read().replace('\n', '')
 			text = text.split("don't()")
 			# At the beginning of the program, mul instructions are enabled
 			first_section = re.findall(r'mul\((\d+)\,(\d+)\)', text[0])
 			for pair in first_section:
 				solution += int(pair[0]) * int(pair[1])
-
+			# For each section beginning with "don't()", only consider the section after the first "do()"
 			for section in text[1:]:
 				if 'do()' in section:
 					enabled_section = ''.join(section.split("do()")[1:])
