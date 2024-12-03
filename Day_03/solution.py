730472bb779d2bd8dd4b952aebe49d6629fc3755
Sam Mosley <sam.mosley@gmail.com>
2024-12-03 10:15:46 +0000

    Solve Day 3

---
+++ Day_03/solution.py
@@ -0,0 +1,39 @@
+import os
+import time
+import re
+
+def main(part):
+	solution = 0
+	dir_path = os.path.dirname(os.path.realpath(__file__))
+	with open(f"{dir_path}/input.txt", "r") as f:
+		if part == 1:
+			while(len(line := f.readline()) != 0):
+				# Find all occurrences of 'mul(x,y)' where x and y are numbers
+				valid_formats = re.findall(r'mul\((\d+)\,(\d+)\)', line)
+				for pair in valid_formats:
+					solution += int(pair[0]) * int(pair[1])
+			return solution
+		else:
+			text = f.read().replace('\n', '')
+			text = text.split("don't()")
+			# At the beginning of the program, mul instructions are enabled
+			first_section = re.findall(r'mul\((\d+)\,(\d+)\)', text[0])
+			for pair in first_section:
+				solution += int(pair[0]) * int(pair[1])
+
+			for section in text[1:]:
+				if 'do()' in section:
+					enabled_section = ''.join(section.split("do()")[1:])
+					valid_formats = re.findall(r'mul\((\d+)\,(\d+)\)', enabled_section)
+					for pair in valid_formats:
+						solution += int(pair[0]) * int(pair[1])
+			return solution
+
+
+if __name__ == "__main__":
+	start_time = time.time()
+	print(f"Part 1 solution: {main(1)}")
+	print("Part 1 finished in %s seconds" % (time.time() - start_time))
+	mid_time = time.time()
+	print(f"Part 2 solution: {main(2)}")
+	print("Part 2 finished in %s seconds" % (time.time() - mid_time))
