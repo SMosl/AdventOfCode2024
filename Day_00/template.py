6dda7649f1bc160ad7901db831b737aeadd82401
Sam Mosley <sam.mosley@gmail.com>
2024-12-09 12:25:24 +0000

    Update template.py

--- Day_00/template.py
+++ Day_00/template.py
@@ -3,15 +3,15 @@ import time
 
 def main(part):
 
-	solution = part
-	dir_path = os.path.dirname(os.path.realpath(__file__))
-	with open(f"{dir_path}/input.txt", "r") as f:
-		return "test"
+    solution = part
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(f"{dir_path}/input.txt", "r") as f:
+        return "test"
 
 if __name__ == "__main__":
-	start_time = time.time()
-	print(f"Part 1 solution: {main(1)}")
-	print("Part 1 finished in %s seconds" % (time.time() - start_time))
-	mid_time = time.time()
-	print(f"Part 2 solution: {main(2)}")
-	print("Part 2 finished in %s seconds" % (time.time() - mid_time))
+    start_time = time.time()
+    print(f"Part 1 solution: {main(1)}")
+    print("Part 1 finished in %s seconds" % (time.time() - start_time))
+    mid_time = time.time()
+    print(f"Part 2 solution: {main(2)}")
+    print("Part 2 finished in %s seconds" % (time.time() - mid_time))
